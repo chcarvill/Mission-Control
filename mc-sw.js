8c8314ef2f084aafe466322cab1361323600862a
chcarvill <chriscarvill@gmail.com>
2026-09-10 12:56:24 +1000

    Add files via upload

--- mc-sw.js
+++ mc-sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mc-v30-time-conflicts';
+const CACHE = 'mc-v31-notion-links';
 const ASSETS = ['./index.html', './mc-manifest.json', './do-app.js', './ascent-app.js'];
 
 self.addEventListener('install', e => {
